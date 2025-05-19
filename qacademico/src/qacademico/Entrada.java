66649dfebdccf2d451e4c0514ff7f3af7d0ce71f
heitoliv <160533815+heitoliv@users.noreply.github.com>
2025-05-19 17:04:40 -0300

    Add files via upload

--- qacademico/src/qacademico/Entrada.java
+++ qacademico/src/qacademico/Entrada.java
@@ -1,5 +1,6 @@
 package qacademico;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Entrada {
@@ -55,16 +56,42 @@ public class Entrada {
     }
 
     public void cadTurma(Sistema s){
+        Turma turma = new Turma();
+
         String nome = lerLinha("Digite o nome da disciplina:");
         int ano = lerInteiro("Digite o ano da disciplina:");
         int sem = lerInteiro("Digite o semestre da disciplina:");
-        String cpf = lerLinha("Digite o CPF do professor:");
-        int quantAluno = lerInteiro("Digite a quantidade de alunos na disciplina:");
-        for (int i = 0; i < quantAluno; i++) {
-            Aluno aluno = new Aluno();
-            String mat = lerLinha("Digite a matrícula do aluno:");
-        }
+
+
+        Professor prof = lerProf(s);
+        ArrayList<Aluno> alunosTurma = lerAlunos(s);
+
+        turma.setNome(nome);
+        turma.setAno(ano);
+        turma.setSem(sem);
+        turma.setProf(prof);
+        turma.setAlunos(alunosTurma);
+
         int quantAv = lerInteiro("Digite a quantidade de avaliações na disciplina:");
+        for (int i = 0; i < quantAv; i++) {
+            int opcao = lerInteiro("""
+            Escolha uma opção:
+            1) Prova
+            2) Trabalho
+            """);
+            if (opcao == 1){
+                String nomeProva = lerLinha("Informe o nome desta avaliação:");
+                int diaProva = lerInteiro("Digite o dia da prova:");
+                int mesProva = lerInteiro("Digite o mês da prova:");
+                int anoProva = lerInteiro("Digite o ano da prova:");
+                double valorProva = lerDouble("Digite o valor máximo desta avaliação:");
+                int numQuestoes = lerInteiro("Digite o número de questões:");
+                for (Aluno alTurma : alunosTurma){
+
+                }
+            }
+
+        }
 
     }
     /**
@@ -103,7 +130,34 @@ public class Entrada {
     }
 
     private Professor lerProf(Sistema s) {
-        // Imprime uma mensagem ao usuário, lê uma e retorna esta linha
+        // Imprime uma mensagem ao usuário, lê uma linha contendo um double e retorna este double
+        ArrayList<Professor> listaProfs = s.getProfs();
 
+        Professor prof = new Professor();
+        String cpf = lerLinha("Digite o CPF do professor:");
+
+        prof.setCpf(cpf);
+        for (Professor profs : listaProfs){
+            if (profs.cpf.equals(prof.cpf)){
+               return prof;
+            }
+        }
+        return null;
+    }
+
+    private ArrayList<Aluno> lerAlunos(Sistema s){
+        ArrayList<Aluno> listaAlunos = s.getAlunos();
+        int quantAluno = lerInteiro("Digite a quantidade de alunos na disciplina:");
+
+        ArrayList<Aluno> alunosTurma = new ArrayList<>();
+        for (int i = 0; i < quantAluno; i++){
+            String mat = lerLinha("Digite a matrícula do aluno:");
+            for (Aluno al : listaAlunos){
+                if (al.getMat().equals(mat)) {
+                    alunosTurma.add(al);
+                }
+            }
+        }
+        return alunosTurma;
     }
 }
