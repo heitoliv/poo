d21e9361ee9578d3f8490678be41a5c8e69faf9f
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-05-22 21:43:26 -0300

    Update AlunoProva.java

--- qacademico/src/qacademico/AlunoProva.java
+++ qacademico/src/qacademico/AlunoProva.java
@@ -6,19 +6,28 @@ public class AlunoProva {
     private Aluno aluno;
     private ArrayList<Double> notas;
 
-    public Aluno getAluno() {
-        return aluno;
+    public AlunoProva(Aluno a, double[] notasArray) {
+        this.aluno = a;
+        this.notas = new ArrayList<>();
+
+        for (double nota : notasArray) {
+            this.notas.add(nota);
+        }
     }
 
-    public void setAluno(Aluno aluno) {
-        this.aluno = aluno;
+    public double notaTotal() {
+        double total = 0;
+        for (Double nota : notas) {
+            total += nota;
+        }
+        return total;
+    }
+    public Aluno getAluno() {
+        return aluno;
     }
 
     public ArrayList<Double> getNotas() {
         return notas;
     }
 
-    public void setNotas(ArrayList<Double> notas) {
-        this.notas = notas;
-    }
 }
