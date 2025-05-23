f961e7bbd78ec49ba9c6c6f137192cee09530ffe
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-05-22 21:46:41 -0300

    Update Main.java

--- qacademico/src/Main.java
+++ qacademico/src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+package qacademico;
+
+/**
+ * Classe principal
+ * @author Hilario Seibel Junior
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Entrada io = new Entrada();
+        Sistema s = new Sistema();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int op = io.menu();
+
+        while (op != 0) {
+            if (op == 1) {
+                io.cadProf(s);
+            }
+            if (op == 2) {
+                io.cadAluno(s);
+            }
+            if (op == 3) {
+                io.cadTurma(s);
+            }
+            if (op == 4) {
+                s.listarTurmas();
+            }
+
+            op = io.menu();
         }
     }
 }
