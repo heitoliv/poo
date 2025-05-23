65b8c97d84e32b5bab06da51075f5bd34d2ff73d
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-05-23 19:19:18 -0300

    Update Turma.java

--- qacademico/src/qacademico/Turma.java
+++ qacademico/src/qacademico/Turma.java
@@ -38,19 +38,20 @@ public class Turma {
                 somaValorAv += valorMaximo;
 
             }
-            if (somaNotaIndividual > 100) {
+            if (somaNotaIndividual > 100.0) {
                 notaFinal = 100;
+                somaNotaIndividual = 100;
             } else{
                 notaFinal = somaNotaIndividual;
             }
 
-            System.out.println(a.toString() + " (Matrícula: " + a.getMat() + "): "+ notas + " = " + notaFinal);
-
+            System.out.println(a.toString() + " " + notas + " = " + notaFinal);
             somaNotas += somaNotaIndividual;
             nAlunos++;
         }
         double mediaTurma = somaNotas / nAlunos;
         System.out.println("Média da turma: " + mediaTurma);
+        System.out.println();
     }
 
     public String getNome() {
