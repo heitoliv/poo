ecc266599aa46d33ba88a2c86aeb85e3b8f8202a
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-05-22 21:42:45 -0300

    Update Aluno.java

--- qacademico/src/qacademico/Aluno.java
+++ qacademico/src/qacademico/Aluno.java
@@ -3,11 +3,15 @@ package qacademico;
 public class Aluno extends Pessoa{
     private String mat;
 
+    public Aluno(String nome, String cpf, String mat) {
+        super(nome, cpf);
+        this.mat = mat;
+        }
+
+    public String toString() {
+        return nome + " (Matrícula: " + mat + ")";
+    }
     public String getMat() {
         return mat;
-    }
-
-    public void setMat(String mat) {
-        this.mat = mat;
     }
 }
