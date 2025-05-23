3535ac1df235a6d0157fdb3228165c467055e297
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-05-22 21:46:27 -0300

    Update Turma.java

--- qacademico/src/qacademico/Turma.java
+++ qacademico/src/qacademico/Turma.java
@@ -10,51 +10,71 @@ public class Turma {
     private ArrayList<Aluno> alunos;
     private ArrayList<Avaliacao> avs;
 
-    public String getNome() {
-        return nome;
+    public Turma(String nome, int ano, int sem, Professor prof, ArrayList<Aluno> alunos, ArrayList<Avaliacao> avs){
+        this.nome = nome;
+        this.ano = ano;
+        this.sem = sem;
+        this.prof = prof;
+        this.alunos = alunos;
+        this.avs = avs;
+    }
+    public void medias() {
+        System.out.println("Médias da turma " + this.nome + " (" + this.ano + "/" + this.sem + "):");
+        double somaNotas = 0;
+        double nAlunos = 0;
+
+        for (Aluno a : alunos) {
+            double somaNotaIndividual = 0;
+            double somaValorAv = 0;
+            String notas = "";
+            double notaFinal = 0;
+
+            for (Avaliacao av : avs) {
+                double nota = av.nota(a.getCpf());
+                double valorMaximo = av.getValor();
+
+                notas += nota + " ";
+                somaNotaIndividual += nota;
+                somaValorAv += valorMaximo;
+
+            }
+            if (somaNotaIndividual > 100) {
+                notaFinal = 100;
+            } else{
+                notaFinal = somaNotaIndividual;
+            }
+
+            System.out.println(a.toString() + " (Matrícula: " + a.getMat() + "): "+ notas + " = " + notaFinal);
+
+            somaNotas += somaNotaIndividual;
+            nAlunos++;
+        }
+        double mediaTurma = somaNotas / nAlunos;
+        System.out.println("Média da turma: " + mediaTurma);
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public String getNome() {
+        return nome;
     }
 
     public int getAno() {
         return ano;
     }
 
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
-
     public int getSem() {
         return sem;
-    }
-
-    public void setSem(int sem) {
-        this.sem = sem;
     }
 
     public Professor getProf() {
         return prof;
     }
 
-    public void setProf(Professor prof) {
-        this.prof = prof;
-    }
-
     public ArrayList<Aluno> getAlunos() {
         return alunos;
-    }
-
-    public void setAlunos(ArrayList<Aluno> alunos) {
-        this.alunos = alunos;
     }
 
     public ArrayList<Avaliacao> getAvs() {
         return avs;
     }
 
-    public void setAvs(ArrayList<Avaliacao> avs) {
-        this.avs = avs;
-    }
 }
