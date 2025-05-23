4d98475eb9e794ae716e82b8de7e1548fcd4acce
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-05-22 21:45:26 -0300

    Update Prova.java

--- qacademico/src/qacademico/Prova.java
+++ qacademico/src/qacademico/Prova.java
@@ -6,19 +6,26 @@ public class Prova extends Avaliacao{
     private int nQuestoes;
     private ArrayList<AlunoProva> notas;
 
-    public ArrayList<AlunoProva> getNotas() {
-        return notas;
+    public Prova(String nome, Data data, double valor, int nq, ArrayList<AlunoProva> notas) {
+        super(nome, data, valor);
+        this.nQuestoes = nq;
+        this.notas = notas;
     }
 
-    public void setNotas(ArrayList<AlunoProva> notas) {
-        this.notas = notas;
+    public double nota(String cpf) {
+        for (AlunoProva ap : notas) {
+            if (ap.getAluno().getCpf().equals(cpf)) {
+                return ap.notaTotal();
+            }
+        }
+        return 0;
+    }
+    public ArrayList<AlunoProva> getNotas() {
+        return notas;
     }
 
     public int getnQuestoes() {
         return nQuestoes;
     }
 
-    public void setnQuestoes(int nQuestoes) {
-        this.nQuestoes = nQuestoes;
-    }
 }
