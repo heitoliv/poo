1a66260d9092350b16cee79c23612d17c1615761
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-05-22 21:45:49 -0300

    Update Sistema.java

--- qacademico/src/qacademico/Sistema.java
+++ qacademico/src/qacademico/Sistema.java
@@ -7,27 +7,81 @@ public class Sistema {
     private ArrayList<Aluno> alunos;
     private ArrayList<Turma> turmas;
 
+    public Sistema(){
+        this.profs = new ArrayList<>();
+        this.alunos = new ArrayList<>();
+        this.turmas = new ArrayList<>();
+    }
+
+
+    public void novoProf(Professor p) {
+        profs.add(p);
+    }
+    public void novoAluno(Aluno a) {
+        alunos.add(a);
+    }
+    public void novaTurma(Turma t) {
+        turmas.add(t);
+    }
+    public Professor encontrarProf(String cpf) {
+        for (Professor p : profs) {
+            if (p.getCpf().equals(cpf)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public Aluno encontrarAluno(String mat) {
+        for (Aluno a : alunos) {
+            if (a.getMat().equals(mat)) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public void listarProfs() {
+
+        if (this.profs != null) {
+            System.out.println("Professores cadastrados:");
+            for (Professor p : this.profs) {
+                System.out.println("* " + p);
+            }
+        } else {
+            System.out.println("Nenhum professor cadastrado até o momento.");
+        }
+    }
+
+    public void listarAlunos() {
+        if (this.alunos.size() > 0) {
+            System.out.println("Alunos cadastrados:");
+            for (Aluno a : this.alunos) {
+                System.out.println("* " + a);
+            }
+        } else {
+            System.out.println("Nenhum aluno cadastrado até o momento.");
+        }
+    }
+
+    public void listarTurmas() {
+        if (this.turmas.size() > 0) {
+            System.out.println("Turmas cadastradas:");
+            for (Turma t : this.turmas) {
+                t.medias();
+            }
+        } else {
+            System.out.println("Nenhuma turma cadastrada até o momento.");
+        }
+    }
+
     public ArrayList<Professor> getProfs() {
         return profs;
     }
-
-    public void setProfs(ArrayList<Professor> profs) {
-        this.profs = profs;
-    }
-
     public ArrayList<Aluno> getAlunos() {
         return alunos;
     }
-
-    public void setAlunos(ArrayList<Aluno> alunos) {
-        this.alunos = alunos;
-    }
-
     public ArrayList<Turma> getTurmas() {
         return turmas;
-    }
-
-    public void setTurmas(ArrayList<Turma> turmas) {
-        this.turmas = turmas;
     }
 }
