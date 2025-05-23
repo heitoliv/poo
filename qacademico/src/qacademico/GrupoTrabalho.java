ef2e81d92ccf9699d760f5afb4b91f298cedd241
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-05-22 21:44:41 -0300

    Update GrupoTrabalho.java

--- qacademico/src/qacademico/GrupoTrabalho.java
+++ qacademico/src/qacademico/GrupoTrabalho.java
@@ -6,11 +6,20 @@ public class GrupoTrabalho {
     ArrayList<Aluno> alunos;
     private double nota;
 
+    public GrupoTrabalho(ArrayList<Aluno> alunos, double nota) {
+        this.alunos = alunos;
+        this.nota = nota;
+    }
+
     public double getNota() {
         return nota;
     }
-
-    public void setNota(double nota) {
-        this.nota = nota;
+    public boolean alunoNoGrupo(String cpf) {
+        for (Aluno a : alunos) {
+            if (a.getCpf().equals(cpf)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
