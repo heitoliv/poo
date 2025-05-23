de22bd73fefdf109f739d0de571a07fffa5a4586
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-05-22 21:44:00 -0300

    Update Data.java

--- qacademico/src/qacademico/Data.java
+++ qacademico/src/qacademico/Data.java
@@ -5,27 +5,35 @@ public class Data {
     private int mes;
     private int ano;
 
+    public Data (int d, int m, int a){
+        this.dia = d;
+        this.mes = m;
+        this.ano = a;
+    }
+
+    boolean posterior(Data d2) {
+        if (this.ano > d2.ano) {
+            return true;
+        }else if (this.ano == d2.ano) {
+            if (this.mes > d2.mes) {
+                return true;
+            }else if (this.mes == d2.mes && this.dia > d2.dia) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public int getDia() {
         return dia;
     }
 
-    public void setDia(int dia) {
-        this.dia = dia;
-    }
 
     public int getMes() {
         return mes;
     }
 
-    public void setMes(int mes) {
-        this.mes = mes;
-    }
-
     public int getAno() {
         return ano;
-    }
-
-    public void setAno(int ano) {
-        this.ano = ano;
     }
 }
