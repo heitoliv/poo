71b5a69152f8bb9a10d333c1acc0c590761cbad1
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-05-22 21:45:15 -0300

    Update Professor.java

--- qacademico/src/qacademico/Professor.java
+++ qacademico/src/qacademico/Professor.java
@@ -3,11 +3,13 @@ package qacademico;
 public class Professor extends Pessoa{
     private double salario;
 
+    public Professor(String nome, String cpf, double salario) {
+        super(nome, cpf);
+        this.salario = salario;
+    }
+
     public double getSalario() {
         return salario;
     }
 
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
 }
